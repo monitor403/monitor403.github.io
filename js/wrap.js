8460b3ccb49398c4888c4dda78afbfbf51457bbc
monitor403 <monitor200@outlook.com>
2020-02-16 18:01:34 +0800

    Site updated: 2020-02-16 18:01:33

--- js/wrap.js
+++ js/wrap.js
@@ -1,7 +1,9 @@
-$("img").not('.page__logo img').each(function () {
+$(document).ready(function(){
+      $("img").not('.page__logo img').each(function () {
       // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
-       var element = document.createElement("a");
-       $(element).attr("data-fancybox", "gallery");
-       $(element).attr("href", $(this).attr("src"));
-       $(this).wrap(element);
+            var element = document.createElement("a");
+             $(element).attr("data-fancybox", "gallery");
+            $(element).attr("href", $(this).attr("src"));
+            $(this).wrap(element);
    });
+});
